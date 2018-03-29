531e7788321575ab8b1ede60ac00a3b7f84a5c7f
Yasir Mohamed <mohamey@tcd.ie>
2018-03-29 19:38:13 +0100

    Changed to select users with most ratings

--- create_blc_input.py
+++ create_blc_input.py
@@ -14,18 +14,27 @@ user_track_tuples = {}
 print("Loading in pickle")
 with open(pickle_path, 'rb') as pickle_file:
     user_track_tuples = load(pickle_file)
-
 print("Done")
+
+num_to_take = len(user_track_tuples)
+if argv[2]: num_to_take = int(argv[2])
+
+sorted_users = sorted(user_track_tuples, key=lambda k: len(user_track_tuples[k]), reverse=True)
 
 def scale_rating(rating):
     return 1 + (rating * (SCALE - 1))
 
 with open('blc_input/users.txt', 'w') as users, open('blc_input/songs.txt', 'w') as songs, open('blc_input/ratings.txt', 'w') as ratings:
     print("Iterating through users")
-    for user, tuples in user_track_tuples.items():
-        for (song, rating) in tuples:
+    count = 0
+    for user in sorted_users:
+        for (song, rating) in user_track_tuples[user]:
             users.write("{}\n".format(str(user)))
             songs.write("{}\n".format(str(song)))
             ratings.write("{}\n".format(str(scale_rating(rating))))
+
+        count += 1
+        if count == num_to_take:
+            break
 
     print("Done")
