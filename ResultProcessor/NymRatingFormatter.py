0c68d124cdfc64da342c59a2f0df9414a1a78cc0
Yasir Mohamed <mohamey@tcd.ie>
2018-04-29 15:21:43 +0100

    Now outputs nyms and nym ratings to be read by db

--- ResultProcessor/NymRatingFormatter.py
+++ ResultProcessor/NymRatingFormatter.py
@@ -3,6 +3,7 @@
 import csv
 from os import listdir, path
 from pickle import dump, load
+from queue import PriorityQueue
 from spotify import SpotifyWrapper
 
 # Read in Nym Ratings
@@ -13,6 +14,7 @@ class NymRatingFormatter:
         self.nym_variance_path = path.join(config["nym_data"]["base"], config["nym_data"]["nym_variance_dir"])
         self.song_to_uri_path = path.join(config["song_data"]["base"], config["song_data"]["song_to_uri_map"])
         self.db_input_path = path.join(config["database_data"]["base"], config["database_data"]["input_data"])
+        self.nym_db_data_path = path.join(config["database_data"]["base"], config["database_data"]["nym_data"])
         self.config = config
 
         self.songs_files = []
@@ -20,6 +22,7 @@ class NymRatingFormatter:
         self.nym_songs_dict = {}
         self.unique_songs = set()
         self.song_to_uri_dict = {}
+        self.nym_top_ratings = {}
 
     def load_data(self):
         print("Loading in Data")
@@ -58,14 +61,27 @@ class NymRatingFormatter:
         # Go through nym songs dict, generate list of (nym, domain, item, rating, num_votes) tuples
         with open(self.db_input_path, 'w') as db_input_file:
             csv_writer = csv.writer(db_input_file, delimiter=',')
-            csv_writer.writerow(['nym', 'domain', 'item', 'rating', 'num_votes'])
+            csv_writer.writerow(['id', 'nym', 'domain', 'item', 'rating', 'num_votes'])
+
+            rating_id = 0
             for nym, ratings in self.nym_songs_dict.items():
                 print("Generating DB output for nym {}".format(nym))
+                self.nym_top_ratings[nym] = PriorityQueue()
 
                 for song_artist_key, rating, num_users in ratings:
                     uri = self.song_to_uri_dict[song_artist_key]
                     if uri:
-                        csv_writer.writerow([nym, 'spotify.com', uri, rating, num_users])
+                        csv_writer.writerow([rating_id, nym, 'spotify.com', uri, rating, num_users])
+                        self.nym_top_ratings[nym].put((-float(rating), rating_id))
+
+                        rating_id += 1
+
+            # Output each nyms top 20 ratings
+            with open(self.nym_db_data_path, 'w') as output:
+                csv_writer = csv.writer(output, delimiter=',')
+                for nym, pq in self.nym_top_ratings.items():
+                    nym_ratings = [pq.get()[1] for _ in range(10) if not pq.empty()]
+                    csv_writer.writerow([nym] + nym_ratings)
 
         print("Finished Writing out to file")
 
